28d08ced9777f405572dc23dfbb62e5fff6b9daf
root <root@snf-11009.localdomain>
2020-03-29 19:41:27 +0000

    add cmPluginMenus

--- Model/RciamStatsViewer.php
+++ Model/RciamStatsViewer.php
@@ -4,6 +4,21 @@ class RciamStatsViewer extends AppModel
 {
     public $cmPluginType= 'other';
 
+    /**
+     * Expose menu items.
+     *
+     * @ since COmanage Registry v2.0.0
+     * @ return Array with menu location type as key and array of labels, controllers, actions as values.
+     */
+    public function cmPluginMenus() {
+        $this->log(__METHOD__ . '::@', LOG_DEBUG);
+        return array(
+        'coconfig' => array(_txt('ct.rciam_stats_viewers.1') =>
+            array('controller' => 'rciam_stats_viewers',
+                  'action'     => 'edit'))
+        );
+    }
+
     public $validate = array(
         'co_id'=> array(
             'rule' => 'numeric',
